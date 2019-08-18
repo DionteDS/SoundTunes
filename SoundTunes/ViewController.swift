fd12940d944e3adbdaf49c91c082461226f865fd
Dionte <Dionte_sb@yahoo.com>
2019-08-17 19:29:49 -0700

    Added in the sound files. Also add the func playSound to take in the index of the notes array.

--- SoundTunes/ViewController.swift
+++ SoundTunes/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    private var audioPlayer : AVAudioPlayer!
+    private let notes = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    func playSound(noteNumber: String) {
         
     }
-
+    
+    @IBAction func soundButtonTapped(_ sender: UIButton) {
+        playSound(noteNumber: notes[sender.tag - 1])
+    }
+    
 
 }
 
