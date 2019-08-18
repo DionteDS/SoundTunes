4cf052ad1f765bb82009783825c43a47e16d3c94
Dionte <Dionte_sb@yahoo.com>
2019-08-17 19:37:56 -0700

    playSound function will determine if the sound file passed in was a .wav file. And if it is a .wav file play the sound.

--- SoundTunes/ViewController.swift
+++ SoundTunes/ViewController.swift
@@ -11,17 +11,32 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    private var audioPlayer : AVAudioPlayer!
-    private let notes = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    private var audioPlayer : AVAudioPlayer! // AVAudioPlayer object
+    private let notes = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"] // notes.wav files
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     
+    
+    /*
+     This function will take in the index of the notes array, and will test to see if it is a .wav file.
+     If it is then we are able to play the sound, and if not we throw an error.
+    */
     func playSound(noteNumber: String) {
         
+        guard let soundURL = Bundle.main.url(forResource: noteNumber, withExtension: "wav") else { return }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            audioPlayer.play()
+        } catch {
+            print("There was an error with the audio file, \(error).")
+        }
+        
     }
+    
     
     @IBAction func soundButtonTapped(_ sender: UIButton) {
         playSound(noteNumber: notes[sender.tag - 1])
